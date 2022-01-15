4e32e5793d36f7f86c4784623c79c45dee23ec5e
Marcelo Arias <hola@marceloarias.com>
2022-01-15 17:18:45 -0500

    Add Product list for admin screen

--- src/admin/pages/dashboard/DashboardScreen.jsx
+++ src/admin/pages/dashboard/DashboardScreen.jsx
@@ -1,28 +1,48 @@
-import React, { useState, useEffect } from "react";
-import {AgGridColumn, AgGridReact} from 'ag-grid-react';
-import { useDispatch, useSelector } from "react-redux";
-import LoginScreen from "../login/LoginScreen";
+import React, { useState, useEffect } from 'react';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import { useDispatch, useSelector } from 'react-redux';
+import LoginScreen from '../login/LoginScreen';
+
 import { listProducts } from '../../../redux/actions/product';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
+import './DashboardScreen.css';
+
 const DashboardScreen = () => {
+  // TODO: Get logged in function to manage administrator session
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
-  const { products, loading, error } = productList;
+  const { products } = productList;
 
   useEffect(() => {
     dispatch(listProducts());
-}, [dispatch]);
+  }, [dispatch]);
 
   return isLoggedIn ? (
     <LoginScreen />
   ) : (
     <DashboardContainer>
       {/* TODO: Dashboard with edit, modify and delete functions */}
+      <DashboardProductList list={products} />
     </DashboardContainer>
+  );
+};
+
+const DashboardProductList = ({ list }) => {
+  return (
+    <div className="ag-theme-alpine" style={{ height: 300 }}>
+      <AgGridReact
+        rowData={list}
+        defaultColDef={{ sortable: true, filter: true }}
+      >
+        <AgGridColumn field="name"></AgGridColumn>
+        <AgGridColumn field="price"></AgGridColumn>
+        <AgGridColumn field="category"></AgGridColumn>
+      </AgGridReact>
+    </div>
   );
 };
 
